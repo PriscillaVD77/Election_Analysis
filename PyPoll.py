fe5f0cb8e3505f9c62aa657b9f2c60a72a9404af
Priscilla Van Dyke <prisvd77@gmail.com>
2021-09-22 14:53:00 -0400

    Updating pypoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,12 +1,19 @@
-#Copied from https://stackoverflow.com/questions/18589146/how-to-use-encoding-utf-8-py-instead-of-cp1252-py-in-python
 #The data we need to retrieve
 import csv
-File_to_Load='Resources\\election_results.csv'
+import os
+#Assign a variable for the file to load and the path.
+File_to_Load= os.path.join("Resources", "election_results.csv")
+#Assign a filename variable to save the file to a path.
+file_to_save=os.path.join("Analysis", "election_analysis.txt")
+#Open the election results and read the file
 with open(File_to_Load) as election_data:
-#1.The total number of votes cast
+    file_reader = csv.reader(election_data)
+    #read and print the header row
+    headers = next(file_reader)
+    print(headers)
+
+#1.The total number of votes cast 
 #2.A complete list of canidates who received votes
 #3.The percentage of votes each canidate won
 #4.The total number of votes each canidate won
 #5.The winner of the election based on popular vote
-    print(election_data)  
-    
